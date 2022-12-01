75bb6e145098972a99bd1b27ede8e7db03828112
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-01 18:42:31 +0300

    Done

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -40,6 +40,16 @@ RSpec.describe "Students", type: :request do
   #   it 'returns the student with the highest grade' do
   #     get '/students/highest-grade'
 
+  describe "GET /students/highest-grade" do
+    it 'returns the student with the highest grade' do
+      get '/students/highest-grade'
+
+      expect(response.body).to include_json({
+        first_name: 'Idris', last_name: 'Elba', grade: 105
+      })
+    end
+  end
+
   #     expect(response.body).to include_json({ 
   #       first_name: 'Idris', last_name: 'Elba', grade: 105 
   #     })
